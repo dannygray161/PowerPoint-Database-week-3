1dab8e8dc4df81514070c0c0ab729d8725d734c6
Daniel Gray <dieseldan14@gmail.com>
2021-06-11 12:49:15 -0500

    added database

---
+++ Practice Database.sql
@@ -0,0 +1,4 @@
+--create table StudentInfo(SID int primary key, SFirstName varchar(100), SAge int, SClass int, SSection char);
+select * from StudentInfo;
+--insert into StudentInfo(SID, SFirstName, SAge, SClass, SSection)
+--values(1105, 'Newton', 15, 10, 'B');
